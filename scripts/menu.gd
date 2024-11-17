375679b8824b7fea8d5c06e12e14054a1115d4a4
wayared <jarecast@espol.edu.ec>
2024-11-17 00:56:26 -0500

    character and tilemap implemented

---
+++ scripts/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/TileMap.tscn")
+	
+
+
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
